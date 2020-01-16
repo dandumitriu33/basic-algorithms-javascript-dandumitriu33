7ee7a08b5c626a1fcc90aa631de03bca9710d058
dandumitriu33 <dandumitriu33@gmail.com>
2020-01-16 18:01:07 +0200

    Add the fizzbuzz function in py and html

--- fizzbuzz/fizbuzz.py
+++ fizzbuzz/fizbuzz.py
@@ -5,3 +5,19 @@ Write a program that prints the numbers from 1 to 100. But:
  - For numbers which are multiples of both three and five print “FizzBuzz”.
 """
 
+
+def fizzbuzz():
+    i = 1
+    while i <= 100:
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
+        i += 1
+
+
+fizzbuzz()
